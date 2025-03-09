a66f314fea0a719057cb958df607c2f0bc22042c
Chukobyte <michaelsmith8293@gmail.com>
2025-03-09 17:22:18 -0400

    Added third party libs.

--- build.zig
+++ build.zig
@@ -28,13 +28,56 @@ pub fn build(b: *std.Build) void {
         exe.import_memory = true;
     }
 
-    // exe.addCSourceFile(.{ .file = b.path("src/c/window.c") });
-    // exe.addIncludePath(b.path("."));
-    // exe.addIncludePath(b.path("src/c/"));
+    const glad_lib: *std.Build.Step.Compile = try add_glad(b, target, optimize);
+    const stb_image_lib: *std.Build.Step.Compile = try add_stb_image(b, target, optimize);
+    exe.linkLibrary(glad_lib);
+    exe.linkLibrary(stb_image_lib);
 
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_exe.step);
+}
+
+fn add_glad(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !*std.Build.Step.Compile {
+    const glad_lib: *std.Build.Step.Compile = b.addStaticLibrary(.{
+        .name = "glad",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const glad_src_path = "thirdparty/glad";
+    const glad_include_path = "thirdparty";
+
+    glad_lib.addIncludePath(b.path(glad_include_path));
+    glad_lib.addCSourceFile(.{ .file = b.path(glad_src_path ++ "/glad.c") });
+
+    glad_lib.linkLibC();
+
+    glad_lib.installHeader(b.path(glad_include_path ++ "/glad/glad.h"), "glad/glad.h");
+    b.installArtifact(glad_lib);
+
+    return glad_lib;
+}
+
+fn add_stb_image(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !*std.Build.Step.Compile {
+    const stb_image_lib: *std.Build.Step.Compile = b.addStaticLibrary(.{
+        .name = "stb_image",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const stb_image_src_path = "thirdparty/stb_image";
+    const stb_image_include_path = "thirdparty/stb_image";
+
+    stb_image_lib.addIncludePath(b.path(stb_image_include_path));
+    stb_image_lib.addCSourceFile(.{ .file = b.path(stb_image_src_path ++ "/stb_image.c"), .flags = &.{ "-std=c99", "-fno-sanitize=undefined" } });
+
+    stb_image_lib.linkLibC();
+
+    stb_image_lib.installHeader(b.path(stb_image_include_path ++ "/stb_image.h"), "stb_image/stb_image.h");
+    b.installArtifact(stb_image_lib);
+
+    return stb_image_lib;
 }
