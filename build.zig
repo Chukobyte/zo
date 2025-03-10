bea69c29a78512a62a2391c8d50c22e8c4b65ba2
Chukobyte <michaelsmith8293@gmail.com>
2025-03-09 22:50:44 -0400

    Adding zlib, libpng, and freetype dependencies.

--- build.zig
+++ build.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+const thirdparty_path = "thirdparty";
+
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -30,8 +32,10 @@ pub fn build(b: *std.Build) void {
 
     const glad_lib: *std.Build.Step.Compile = try add_glad(b, target, optimize);
     const stb_image_lib: *std.Build.Step.Compile = try add_stb_image(b, target, optimize);
+    const freetype_lib: *std.Build.Step.Compile = try add_freetype(b, target, optimize);
     exe.linkLibrary(glad_lib);
     exe.linkLibrary(stb_image_lib);
+    exe.linkLibrary(freetype_lib);
     exe.linkSystemLibrary("gdi32");
     exe.linkSystemLibrary("user32");
     exe.linkSystemLibrary("opengl32");
@@ -44,17 +48,17 @@ pub fn build(b: *std.Build) void {
 }
 
 fn add_glad(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !*std.Build.Step.Compile {
+    const glad_path = thirdparty_path ++ "/glad";
+    const glad_include_path = thirdparty_path;
+
     const glad_lib: *std.Build.Step.Compile = b.addStaticLibrary(.{
         .name = "glad",
         .target = target,
         .optimize = optimize,
     });
 
-    const glad_src_path = "thirdparty/glad";
-    const glad_include_path = "thirdparty";
-
     glad_lib.addIncludePath(b.path(glad_include_path));
-    glad_lib.addCSourceFile(.{ .file = b.path(glad_src_path ++ "/glad.c") });
+    glad_lib.addCSourceFile(.{ .file = b.path(glad_path ++ "/glad.c") });
 
     glad_lib.linkLibC();
 
@@ -65,17 +69,17 @@ fn add_glad(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.built
 }
 
 fn add_stb_image(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !*std.Build.Step.Compile {
+    const stb_image_path = thirdparty_path ++ "/stb_image";
+    const stb_image_include_path = thirdparty_path ++ "/stb_image";
+
     const stb_image_lib: *std.Build.Step.Compile = b.addStaticLibrary(.{
         .name = "stb_image",
         .target = target,
         .optimize = optimize,
     });
 
-    const stb_image_src_path = "thirdparty/stb_image";
-    const stb_image_include_path = "thirdparty/stb_image";
-
     stb_image_lib.addIncludePath(b.path(stb_image_include_path));
-    stb_image_lib.addCSourceFile(.{ .file = b.path(stb_image_src_path ++ "/stb_image.c"), .flags = &.{ "-std=c99", "-fno-sanitize=undefined" } });
+    stb_image_lib.addCSourceFile(.{ .file = b.path(stb_image_path ++ "/stb_image.c"), .flags = &.{ "-std=c99", "-fno-sanitize=undefined" } });
 
     stb_image_lib.linkLibC();
 
@@ -83,4 +87,219 @@ fn add_stb_image(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.
     b.installArtifact(stb_image_lib);
 
     return stb_image_lib;
+}
+
+fn add_freetype(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !*std.Build.Step.Compile {
+    const ft_path = thirdparty_path ++ "/freetype-VER-2-13-2";
+    const ft_include_path = ft_path ++ "/include";
+    const ft_srcs = [_][]const u8 {
+        ft_path ++ "/src/autofit/autofit.c",
+        ft_path ++ "/src/base/ftbase.c",
+        ft_path ++ "/src/base/ftbbox.c",
+        ft_path ++ "/src/base/ftbdf.c",
+        ft_path ++ "/src/base/ftbitmap.c",
+        ft_path ++ "/src/base/ftcid.c",
+        ft_path ++ "/src/base/ftfstype.c",
+        ft_path ++ "/src/base/ftgasp.c",
+        ft_path ++ "/src/base/ftglyph.c",
+        ft_path ++ "/src/base/ftgxval.c",
+        ft_path ++ "/src/base/ftinit.c",
+        ft_path ++ "/src/base/ftmm.c",
+        ft_path ++ "/src/base/ftotval.c",
+        ft_path ++ "/src/base/ftpatent.c",
+        ft_path ++ "/src/base/ftpfr.c",
+        ft_path ++ "/src/base/ftstroke.c",
+        ft_path ++ "/src/base/ftsynth.c",
+        ft_path ++ "/src/base/fttype1.c",
+        ft_path ++ "/src/base/ftwinfnt.c",
+        ft_path ++ "/src/bdf/bdf.c",
+        ft_path ++ "/src/bzip2/ftbzip2.c",
+        ft_path ++ "/src/cache/ftcache.c",
+        ft_path ++ "/src/cff/cff.c",
+        ft_path ++ "/src/cid/type1cid.c",
+        ft_path ++ "/src/gzip/ftgzip.c",
+        ft_path ++ "/src/lzw/ftlzw.c",
+        ft_path ++ "/src/pcf/pcf.c",
+        ft_path ++ "/src/pfr/pfr.c",
+        ft_path ++ "/src/psaux/psaux.c",
+        ft_path ++ "/src/pshinter/pshinter.c",
+        ft_path ++ "/src/psnames/psnames.c",
+        ft_path ++ "/src/raster/raster.c",
+        ft_path ++ "/src/sdf/sdf.c",
+        ft_path ++ "/src/sfnt/sfnt.c",
+        ft_path ++ "/src/smooth/smooth.c",
+        ft_path ++ "/src/svg/svg.c",
+        ft_path ++ "/src/truetype/truetype.c",
+        ft_path ++ "/src/type1/type1.c",
+        ft_path ++ "/src/type42/type42.c",
+        ft_path ++ "/src/winfonts/winfnt.c",
+    };
+
+    const ft_lib: *std.Build.Step.Compile = b.addStaticLibrary(.{
+        .name = "freetype",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    ft_lib.linkLibC();
+    if (target.result.os.tag == std.Target.Os.Tag.linux) {
+        ft_lib.linkSystemLibrary("m");
+    }
+    const zlib_lib = try add_zlib(b, target, optimize);
+    const libpng_lib = try add_libpng(b, target, optimize, zlib_lib);
+    ft_lib.linkLibrary(zlib_lib);
+    ft_lib.linkLibrary(libpng_lib);
+
+    ft_lib.addIncludePath(b.path(ft_include_path));
+    ft_lib.root_module.addCMacro("FT_BUILD_LIBRARY", "1");
+
+    var flags = std.ArrayList([]const u8).init(b.allocator);
+    defer flags.deinit();
+    try flags.appendSlice(&.{
+        "-DFT2_BUILD_LIBRARY",
+
+        "-DFT_CONFIG_OPTION_SYSTEM_ZLIB=1",
+
+        "-DHAVE_UNISTD_H",
+        "-DHAVE_FCNTL_H",
+
+        "-fno-sanitize=undefined",
+
+        "-DFT_CONFIG_OPTION_USE_PNG=1",
+    });
+
+    const ft_build_path = ft_path ++ "/builds";
+    switch (target.result.os.tag) {
+        .windows => {
+            ft_lib.addCSourceFile(.{ .file = b.path(ft_path ++ "/src/base/fterrors.c"), .flags = flags.items });
+            ft_lib.addCSourceFile(.{ .file = b.path(ft_build_path ++ "/windows/ftdebug.c"), .flags = flags.items });
+            ft_lib.addCSourceFile(.{ .file = b.path(ft_build_path ++ "/windows/ftsystem.c"), .flags = flags.items });
+            ft_lib.addWin32ResourceFile(.{ .file = b.path(ft_path ++ "/src/base/ftver.rc") });
+
+        },
+        .linux => {
+            ft_lib.addCSourceFile(.{ .file = b.path(ft_build_path ++ "/unix/ftsystem.c"), .flags = flags.items });
+            ft_lib.addCSourceFile(.{ .file = b.path(ft_path ++ "/src/base/ftdebug.c"), .flags = flags.items });
+        },
+        else => {
+            ft_lib.addCSourceFile(.{ .file = b.path(ft_path ++ "/src/base/ftsystem.c"), .flags = flags.items });
+            ft_lib.addCSourceFile(.{ .file = b.path(ft_path ++ "/src/base/ftdebug.c"), .flags = flags.items });
+        },
+    }
+
+    ft_lib.addCSourceFiles(.{ .files = &ft_srcs, .flags = flags.items });
+
+    ft_lib.installHeadersDirectory(b.path(ft_include_path), ".", .{});
+    b.installArtifact(ft_lib);
+
+    return ft_lib;
+}
+
+fn add_zlib(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !*std.Build.Step.Compile {
+    const zlib_path = thirdparty_path ++ "/zlib";
+    const zlib_src_path = zlib_path ++ "/src";
+    const zlib_srcs = [_][]const u8 {
+        zlib_src_path ++ "/adler32.c",
+        zlib_src_path ++ "/compress.c",
+        zlib_src_path ++ "/crc32.c",
+        zlib_src_path ++ "/deflate.c",
+        zlib_src_path ++ "/gzclose.c",
+        zlib_src_path ++ "/gzlib.c",
+        zlib_src_path ++ "/gzread.c",
+        zlib_src_path ++ "/gzwrite.c",
+        zlib_src_path ++ "/inflate.c",
+        zlib_src_path ++ "/infback.c",
+        zlib_src_path ++ "/inftrees.c",
+        zlib_src_path ++ "/inffast.c",
+        zlib_src_path ++ "/trees.c",
+        zlib_src_path ++ "/uncompr.c",
+        zlib_src_path ++ "/zutil.c",
+    };
+
+    const zlib_lib: *std.Build.Step.Compile = b.addStaticLibrary(.{
+        .name = "z",
+        .target = target,
+        .optimize = optimize,
+    });
+    zlib_lib.linkLibC();
+    zlib_lib.addIncludePath(b.path(zlib_src_path));
+
+    zlib_lib.installHeadersDirectory(b.path(zlib_src_path), ".", .{ .exclude_extensions = &.{ ".c", ".in", ".txt" } });
+
+    var zlib_flags = std.ArrayList([]const u8).init(b.allocator);
+    defer zlib_flags.deinit();
+    try zlib_flags.appendSlice(&.{
+        "-DHAVE_SYS_TYPES_H",
+        "-DHAVE_STDINT_H",
+        "-DHAVE_STDDEF_H",
+        "-DZ_HAVE_UNISTD_H",
+    });
+    zlib_lib.addCSourceFiles(.{ .files = &zlib_srcs, .flags = zlib_flags.items });
+
+    b.installArtifact(zlib_lib);
+
+    return zlib_lib;
+}
+
+fn add_libpng(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, zlib_lib: *std.Build.Step.Compile) !*std.Build.Step.Compile {
+    const libpng_path = thirdparty_path ++ "/libpng";
+    const libpng_headers = [_][]const u8 {
+        "png.h",
+        "pngconf.h",
+        "pngdebug.h",
+        "pnginfo.h",
+        "pngpriv.h",
+        "pngstruct.h",
+    };
+    const libpng_srcs = [_][]const u8 {
+        libpng_path ++ "/src/png.c",
+        libpng_path ++ "/src/png.c",
+        libpng_path ++ "/src/pngerror.c",
+        libpng_path ++ "/src/pngget.c",
+        libpng_path ++ "/src/pngmem.c",
+        libpng_path ++ "/src/pngpread.c",
+        libpng_path ++ "/src/pngread.c",
+        libpng_path ++ "/src/pngrio.c",
+        libpng_path ++ "/src/pngrtran.c",
+        libpng_path ++ "/src/pngrutil.c",
+        libpng_path ++ "/src/pngset.c",
+        libpng_path ++ "/src/pngtrans.c",
+        libpng_path ++ "/src/pngwio.c",
+        libpng_path ++ "/src/pngwrite.c",
+        libpng_path ++ "/src/pngwtran.c",
+        libpng_path ++ "/src/pngwutil.c",
+    };
+
+    const libpng_lib: *std.Build.Step.Compile = b.addStaticLibrary(.{
+        .name = "png",
+        .target = target,
+        .optimize = optimize,
+    });
+    libpng_lib.linkLibC();
+    if (target.result.os.tag == std.Target.Os.Tag.linux) {
+        libpng_lib.linkSystemLibrary("m");
+    }
+
+    libpng_lib.linkLibrary(zlib_lib);
+    libpng_lib.addIncludePath(b.path(libpng_path ++ "/include"));
+    libpng_lib.addIncludePath(b.path(libpng_path ++ "/src"));
+
+    var libpng_flags = std.ArrayList([]const u8).init(b.allocator);
+    defer libpng_flags.deinit();
+    try libpng_flags.appendSlice(&.{
+        "-DPNG_ARM_NEON_OPT=0",
+        "-DPNG_POWERPC_VSX_OPT=0",
+        "-DPNG_INTEL_SSE_OPT=0",
+        "-DPNG_MIPS_MSA_OPT=0",
+    });
+    libpng_lib.addCSourceFiles(.{ .files = &libpng_srcs, .flags = libpng_flags.items });
+
+    libpng_lib.installHeader(b.path(libpng_path ++ "/include/pnglibconf.h"), "pnglibconf.h");
+    inline for (libpng_headers) |header| {
+        libpng_lib.installHeader(b.path(libpng_path ++ "/src/" ++ header), header);
+    }
+
+    b.installArtifact(libpng_lib);
+
+    return libpng_lib;
 }
