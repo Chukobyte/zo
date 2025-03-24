ce0f7cf4b619991889cdb9138903b32cafa3fb18
Chukobyte <michaelsmith8293@gmail.com>
2025-03-23 22:23:04 -0400

    Updates.

--- build.zig
+++ build.zig
@@ -330,7 +330,7 @@ fn add_libpng(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.bui
 }
 
 fn add_zo_audio(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !*std.Build.Step.Compile {
-    const c_src = "src/c";
+    const c_src = "src/zo/c";
     const miniaudio_path = thirdparty_path ++ "/miniaudio";
 
     const audio_lib: *std.Build.Step.Compile = b.addStaticLibrary(.{
@@ -342,7 +342,7 @@ fn add_zo_audio(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.b
     // Add miniaudio directory to be included
     audio_lib.addIncludePath(b.path(c_src));
     audio_lib.addIncludePath(b.path(miniaudio_path));
-    audio_lib.addCSourceFile(.{.file = b.path("src/c/zo_audio.c")});
+    audio_lib.addCSourceFile(.{.file = b.path("src/zo/c/zo_audio.c")});
     
     audio_lib.installHeadersDirectory(b.path(c_src), ".", .{});
 
